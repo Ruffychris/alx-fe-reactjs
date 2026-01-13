46d2cbf5cec1d7cd0c117c37b48bdc532140320f
Ruffychris <christian.rufus23@gmail.com>
2026-01-13 10:25:39 +0000

    Crate specific components in a React app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 function App() {
     return (
         <div>
+            <WelcomeMessage />
             <Header />
             <MainContent />
             <Footer />
