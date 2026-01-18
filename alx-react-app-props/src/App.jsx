6e52b6bf40e89b7cf68a7d8986b8f9369bd23748
Ruffychris <christian.rufus23@gmail.com>
2026-01-18 22:24:34 +0000

    refactoring prop drilling to use context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,28 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import ProfilePage from './components/ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
-    return (
-        <div>
-            <WelcomeMessage />
-            <Header />
-            <MainContent />
-            <Footer />
-            <UserProfile 
-                name="Alice" 
-                age={25} 
-                bio="Loves hiking and photography" 
-            />
-            <UserProfile 
-                name="Bob" 
-                age={30} 
-                bio="Enjoys cooking and traveling" 
-            />
-        </div>
-    );
+  const userData = {
+    name: 'Rufus Christian',
+    email: 'christian.rufus23@gmail.com'
+  };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
