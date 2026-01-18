a21d8b9f2a0f08c4ec442cf9a19181f436bd767e
Ruffychris <christian.rufus23@gmail.com>
2026-01-18 22:56:30 +0000

    aa

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,16 @@
-import ProfilePage from './ProfilePage';
+import ProfilePage from './components/ProfilePage';
+import UserContext from '../UserContext'; // root-level import
 
 function App() {
   const userData = {
-    name: "Jane Doe",
-    email: "jane.doe@example.com"
+    name: 'Jane Doe',
+    email: 'jane.doe@example.com'
   };
 
   return (
-    <ProfilePage userData={userData} />
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
