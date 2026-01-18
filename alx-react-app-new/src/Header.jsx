a583f35055d229d606153c9b82aab2e2b1149fcd
Ruffychris <christian.rufus23@gmail.com>
2026-01-18 20:25:35 +0000

    adde inline stylien to header component

--- alx-react-app-new/src/Header.jsx
+++ alx-react-app-new/src/Header.jsx
@@ -1,9 +1,17 @@
-function Header() {
-    return (
-        <header style = {{backgroundColor: 'navy', color: 'white', textAlign: 'center'}}>
-            <h1>My Favorite Cities</h1>
-        </header>
-    );
-}
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{
+      backgroundColor: 'navy',
+      color: 'white',
+      textAlign: 'center',
+      padding: '20px',
+      borderRadius: '5px'
+    }}>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+};
 
 export default Header;
