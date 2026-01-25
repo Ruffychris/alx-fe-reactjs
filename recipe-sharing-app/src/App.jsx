5c1b90189eea76e36e81e908b9599e0bc542ab71
Ruffychris <christian.rufus23@gmail.com>
2026-01-25 00:54:37 +0000

    enhance recipe-sharing app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,19 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 const App = () => {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
+
       <AddRecipeForm />
-      <RecipeList />
+
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
     </div>
   );
 };
