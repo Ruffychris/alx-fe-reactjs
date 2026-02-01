6879be5c0230526578a523d55f9ad0882ae9ac08
Ruffychris <christian.rufus23@gmail.com>
2026-02-01 22:27:56 +0000

    enhancing user interface

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,28 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(
-      `https://api.github.com/users/${username}`
-    );
+const BASE_URL = "https://api.github.com";
 
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+// Old single user fetch
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`${BASE_URL}/users/${username}`);
+  return response.data;
+};
+
+// Advanced search for multiple users
+export const searchUsers = async (username, location, minRepos, page = 1) => {
+  // Build query
+  let query = "";
+  if (username) query += `${username}`;
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
+
+  const response = await axios.get(`${BASE_URL}/search/users`, {
+    params: {
+      q: query,
+      page,
+      per_page: 10,
+    },
+  });
+
+  return response.data; // Note: response.data.items contains array of users
 };
