8b0f7e09b02fc17ca542e9b4db76cb6d22bc4350
Ruffychris <christian.rufus23@gmail.com>
2026-02-09 01:23:08 +0000

    few changes made

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // ✅ includes react-router-dom & BrowserRouter
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 import AddRecipeForm from "./components/AddRecipeForm";
 
-<Routes>
-  <Route path="/" element={<HomePage />} />
-  <Route path="/recipe/:id" element={<RecipeDetail />} />
-  <Route path="/add" element={<AddRecipeForm />} />
-</Routes>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
