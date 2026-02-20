96e86ac293f801fa4799927b42390c5a1be90fd7
Ruffychris <christian.rufus23@gmail.com>
2026-02-20 22:06:55 +0000

    Fixed the App.jsx file

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,19 +1,25 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { useState } from "react";
 import PostsComponent from "./components/PostsComponent";
+
+// Create Query Client
+const queryClient = new QueryClient();
 
 function App() {
   const [showPosts, setShowPosts] = useState(true);
 
   return (
-    <div>
-      <h1>React Query Demo</h1>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
 
-      <button onClick={() => setShowPosts(!showPosts)}>
-        {showPosts ? "Hide Posts" : "Show Posts"}
-      </button>
+        <button onClick={() => setShowPosts(!showPosts)}>
+          {showPosts ? "Hide Posts" : "Show Posts"}
+        </button>
 
-      {showPosts && <PostsComponent />}
-    </div>
+        {showPosts && <PostsComponent />}
+      </div>
+    </QueryClientProvider>
   );
 }
 
