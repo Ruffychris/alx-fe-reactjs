5dfc0b0e1f174496b045fd4d3771a0a34f82b19c
Ruffychris <christian.rufus23@gmail.com>
2026-02-22 01:00:07 +0000

    fixed a bug in Profile.jsx file

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
   return (
@@ -10,8 +12,11 @@ const Profile = () => {
         <Link to="settings">Settings</Link>
       </nav>
 
-      {/* Nested routes render here */}
-      <Outlet />
+      {/* Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 };
